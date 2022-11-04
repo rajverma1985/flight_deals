a90a447ec5d8a8e51f5ea86afc4f1ecfdc370de4
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-11-04 23:09:26 +0530

    restructure app

--- app/search/notification_manager.py
+++ app/search/notification_manager.py
@@ -1,3 +1,9 @@
 class NotificationManager:
+    def __init__(self):
+        pass
+
+    @property
+    def something(self):
+        pass
     #This class is responsible for sending notifications with the deal flight details.
     pass
